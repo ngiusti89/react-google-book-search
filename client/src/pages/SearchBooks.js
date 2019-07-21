8b85a7c8c3ee7aa898a266923ce4bfe367de9f6c
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-07-20 22:13:45 -0700

    books state

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,3 +3,13 @@ import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
 import { Container, Row, Col } from "../components/Grid";
 import SearchForm from "../components/SearchForm";
+
+class Books extends Component {
+state = {
+    books: [],
+    search: "",
+    title: "",
+    author: "",
+    description: ""
+};
+}
